87c064e0ae0e5952f96584cba53412356e32ad5e
hutamy <hutamytriesthi@gmail.com>
2020-12-16 13:29:56 +0700

    add gradient in facilities

--- store/actions/actionHotels.js
+++ store/actions/actionHotels.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = "http://192.168.100.7:3002"
+const baseUrl = "http://192.168.1.15:3001"
 
 export const fetchHotelById = (id) => {
     return (dispatch) => {
